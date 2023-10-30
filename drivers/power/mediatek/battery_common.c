4b04fe1262526c60e984b912136e13af61287fb5
R0rt1z2 <me@r0rt1z2.com>
2023-10-30 17:49:45 +0100

    power: battery: Add `CONFIG_AMAZON_SIGN_OF_LIFE` guard
    * Oh, Amazon...

--- drivers/power/mediatek/battery_common.c
+++ drivers/power/mediatek/battery_common.c
@@ -3626,7 +3626,9 @@ static void mt_battery_thermal_check(void)
 		/* ignore default rule */
 		if (BMT_status.temperature <= -10) {
 			pr_notice("[Battery] Tbat(%d)<= -10, system need power down.\n", BMT_status.temperature);
+#ifdef CONFIG_AMAZON_SIGN_OF_LIFE
 			life_cycle_set_thermal_shutdown_reason(THERMAL_SHUTDOWN_REASON_BATTERY);
+#endif
 			orderly_poweroff(true);
 		}
 #else
